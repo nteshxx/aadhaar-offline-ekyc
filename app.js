e10afc2fbf2e623902ce2068d00b482063bf78c6
Nitesh Yadav <60145175+nteshxx@users.noreply.github.com>
2021-10-04 22:15:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html'); 
 });
 
+app.get('/getip', (req, res) => {
+    const forwarded = req.headers['x-forwarded-for'];
+    const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+    console.log(ip);
+    res.status(200).json(ip)
+})
+
 app.use(require('./routes/verify'))
  
 app.listen(PORT, () => console.log('Server started on port ', PORT));
